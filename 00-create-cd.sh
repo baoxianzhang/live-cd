0f35cc8104224250ad68b6309abf8662b028e2a7
baoxianzhang <baoxianzhit@gmail.com>
2018-01-14 09:01:45 +0800

    update contain

--- 00-create-cd.sh
+++ 00-create-cd.sh
@@ -17,7 +17,7 @@ if [ $? != 0 ]; then
     usage
 fi
 
-ROSDISTRO=hydro
+ROSDISTRO=indigo
 eval set -- $OPT
 while [ -n "$1" ] ; do
     echo $1
@@ -31,8 +31,8 @@ while [ -n "$1" ] ; do
 done
 
 case $ROSDISTRO in
-    hydro ) ISO=ubuntu-12.04.5-desktop-amd64.iso;;
     indigo) ISO=ubuntu-14.04.5-desktop-amd64.iso;;
+    kinetic) ISO=ubuntu-16.04.3-desktop-amd64.iso;;
     *) echo "[ERROR] Unsupported ROSDISTRO $ROSDISTRO"; exit;;
 esac
 REV=`echo ${ISO} | sed "s/ubuntu-\([0-9]*.[0-9]*\).*/\\1/"`
@@ -75,21 +75,13 @@ cat /etc/apt/sources.list
 
 # install ros
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu \`lsb_release -cs\` main" > /etc/apt/sources.list.d/ros-latest.list'
-wget http://packages.ros.org/ros.key -O - | apt-key add -
+# apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 apt-get update
 echo "hddtemp hddtemp/daemon boolean false" | sudo debconf-set-selections
 apt-get -y -q install ros-$ROSDISTRO-desktop-full ros-$ROSDISTRO-catkin  ros-$ROSDISTRO-rosbash
 apt-get -y -q install python-wstool python-rosdep python-catkin-tools
-apt-get -y -q install aptitude git ntp emacs vim
-
-# mongodb hack for 14.04
-if [ ${ROSDISTRO} == "indigo" ]; then
-  echo "mongodb:x:130:65534::/home/mongodb:/bin/false" >> /etc/passwd
-  echo "mongodb:x:130:mongodb" >> /etc/group
-  apt-get install -q -y mongodb mongodb-clients mongodb-server -o Dpkg::Options::="--force-confdef" || echo "ok"
-  sed -i -r 's/invoke-rc.d/#&/' /var/lib/dpkg/info/mongodb-server.postinst
-  apt-get install -q -y mongodb mongodb-clients mongodb-server -o Dpkg::Options::="--force-confdef" || echo "ok"
-fi
 
 # rosdep
 rosdep init; rosdep update || echo "ok"
@@ -113,13 +105,6 @@ mkdir -p /home/ubuntu/catkin_ws/src
 cd /home/ubuntu/catkin_ws
 wstool init src || echo "already initilized"
 
-# setup ros_tutorials
-wstool set -t src roscpp_tutorials https://github.com/ros/ros_tutorials.git --git -y || echo "already configured"
-# for baxter seminar
-wstool merge -t src https://raw.github.com/tork-a/baxter_seminar/master/baxter_seminar.rosinstall
-# For TORK ROS workshop
-wstool set -t src tork-a/ros_seminar https://github.com/tork-a/ros_seminar.git --git -y || echo "tork-a/ros_seminar already configured"
-
 # update and install
 wstool update -t src
 rosdep install -r -n -y --rosdistro $ROSDISTRO --from-paths src --ignore-src
@@ -129,59 +114,11 @@ catkin build -p 1 --no-status
 cd -
 chown -R 999.999 /home/ubuntu/catkin_ws
 
-# For turtlebot
-apt-get -y -q install ros-$ROSDISTRO-turtlebot-simulator
-apt-get -y -q install ros-$ROSDISTRO-turtlebot-apps
-apt-get -y -q install ros-$ROSDISTRO-turtlebot
-# For kobuki
-apt-get -y -q install ros-$ROSDISTRO-kobuki-desktop
-# For devices
-apt-get -y -q install ros-$ROSDISTRO-dynamixel-motor
-apt-get -y -q install ros-$ROSDISTRO-libuvc-camera
-apt-get -y -q install ros-$ROSDISTRO-uvc-camera
-apt-get -y -q install ros-$ROSDISTRO-ar-track-alvar
-apt-get -y -q install ros-$ROSDISTRO-openni2-launch
-apt-get -y -q install ros-$ROSDISTRO-audio-common
-# For moveit
-apt-get -y -q install ros-$ROSDISTRO-moveit-ikfast
-apt-get -y -q install ros-$ROSDISTRO-moveit-full-pr2
-# qt-build
-apt-get -y -q install ros-$ROSDISTRO-qt-build
-# For Denso
-apt-get -y -q install ros-$ROSDISTRO-denso
-# For Nextage
-apt-get -y -q install ros-$ROSDISTRO-rtmros-hironx ros-$ROSDISTRO-rtmros-nextage
-
-if [ ${ROSDISTRO} == "hydro" ]; then
-# RTM, Hiro-NXO
-#apt-get -y -q install ros-$ROSDISTRO-hironx-tutorial
-# For turtlebot
-apt-get -y -q install ros-$ROSDISTRO-turtlebot-viz
-# For moveit
-apt-get -y -q install ros-$ROSDISTRO-industrial-desktop
-
-#rosemacs
-apt-get -y -q install rosemacs-el
-fi # hydro
-if [ ${ROSDISTRO} == "indigo" ]; then
-apt-get -y -q install ros-$ROSDISTRO-rosemacs
-
-# hakuto
-apt-get -y -q install ros-$ROSDISTRO-hakuto
-fi
-
-
-# install chromium
-apt-get -y -q install chromium-browser
-
-# install gnome-open
-apt-get -y -q install libgnome2.0
+# install nautilus-open-terminal
+apt-get -y -q nautilus-open-terminal
 
 # install freecad
 apt-get -y -q install freecad
-
-# for japanese environment
-apt-get -y -q install language-pack-gnome-ja latex-cjk-japanese xfonts-intl-japanese
 
 # fix resolve conf (https://github.com/tork-a/live-cd/issues/8)
 rm -fr /etc/resolv.conf
@@ -213,9 +150,6 @@ options=['ctrl\tctrl:nocaps']
 # add gnome-terminal icon
 dbus-launch --exit-with-session gsettings set com.canonical.Unity.Launcher favorites "\$(gsettings get com.canonical.Unity.Launcher favorites | sed "s/, *'gnome-terminal.desktop' *//g" | sed "s/'gnome-terminal.desktop' *, *//g" | sed -e "s/]$/, 'gnome-terminal.desktop']/")"
 gsettings get com.canonical.Unity.Launcher favorites
-
-# add chromium-browser.desktop
-dbus-launch --exit-with-session gsettings set com.canonical.Unity.Launcher favorites "\$(gsettings get com.canonical.Unity.Launcher favorites | sed "s/, *'chromium-browser.desktop' *//g" | sed "s/'chromium-browser.desktop' *, *//g" | sed -e "s/]$/, 'chromium-browser.desktop']/")"
 
 echo "
 [com.canonical.Unity.Launcher]
@@ -254,13 +188,13 @@ if [ ! ${DEBUG} ]; then
 
     # create iso
     if [ ${ENABLE_LOCAL_REPOSITORY} ]; then
-        FILENAME=tork-ubuntu-ros-${REV}-local-repo-amd64-${DATE}.iso
+        FILENAME=baoxian-ubuntu-ros-${REV}-local-repo-amd64-${DATE}.iso
     else
-        FILENAME=tork-ubuntu-ros-${REV}-amd64-${DATE}.iso
+        FILENAME=baoxian-ubuntu-ros-${REV}-amd64-${DATE}.iso
     fi
     DATE=`date +%m%d`
                                               #1234 56789012345 678901 2 3 456789012
-    sudo uck-remaster-pack-iso $FILENAME -g -d=TORK\ Ubuntu/ROS\ Linux\ \(${DATE}\)
+    sudo uck-remaster-pack-iso $FILENAME -g -d=BAO\ Ubuntu/ROS\ Linux\ \(${DATE}\)
     sudo cp -f ~/tmp/remaster-new-files/$FILENAME .
     sudo chown jenkins.jenkins $FILENAME
 fi
